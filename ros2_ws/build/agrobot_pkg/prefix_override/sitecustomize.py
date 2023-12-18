82624a4838dfbaa136ede85bf6ae9f6b8bdc7ef1
Tom Jacobs <tomjacobs2003@hotmail.com>
2023-12-18 12:20:58 +0100

    Add CAN ODrive package
    update .gitignore

--- ros2_ws/build/agrobot_pkg/prefix_override/sitecustomize.py
+++ ros2_ws/build/agrobot_pkg/prefix_override/sitecustomize.py
@@ -1,3 +1,3 @@
 import sys
 sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/tjacobs/Documents/Agrobot_ws/ros2_ws/install/agrobot_pkg'
+sys.prefix = sys.exec_prefix = '/home/tjacobs/Agrobot_ws/ros2_ws/install/agrobot_pkg'
