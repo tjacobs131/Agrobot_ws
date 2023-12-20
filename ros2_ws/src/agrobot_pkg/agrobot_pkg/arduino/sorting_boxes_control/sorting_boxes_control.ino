057801a0e280fe06068c96aeef87cbf7398acc69
Tom Jacobs <tomjacobs2003@hotmail.com>
2023-12-20 13:03:33 +0100

    arduino excecutes commands

--- ros2_ws/src/agrobot_pkg/agrobot_pkg/arduino/sorting_boxes_control/sorting_boxes_control.ino
+++ ros2_ws/src/agrobot_pkg/agrobot_pkg/arduino/sorting_boxes_control/sorting_boxes_control.ino
@@ -1,144 +1,129 @@
-#include "Arduino_EMBRYO_2.h"
-
 #define OPEN 1
 #define CLOSE 0
+
+#define speedDelay 2
+#define openPuls 900
+
+#define mBackwards -1
+#define mForwards 1
+#define mStop 0
+
+#define motorCount 4
 
 #define buffSize 256
 char receiveBuffer[buffSize];
 uint8_t receiveBufferSize = 0;
 
-const int BackwardPin = 37;   // Backward Button X-axis
-const int ForwardPin = 37;    // Forward Button X-axis
-const int startPin = 37;      // Start Button
-const int emergencyPin = 37;  // Emergency Button
-const int FarEndstop = 37;    // Far From Home endstop X-axis
+const int enablePin[]  = {38, A2, A8, 24};
+const int dirPin[]     = {A1, A7, 48, 28};
+const int stepPin[]    = {A0, A6, 46, 26};
+const int endstopPin[] = {15, 14, 02, 03};
+unsigned long lastStepTime [motorCount];
+int motorPosition [motorCount];
+int motorMove [motorCount];
+bool motorPulse [motorCount];
+int targetPosition [motorCount];
 
-
-#define motor01 1
-
-const int enablePin1 = 38;  // Enable Pin
-const int DirPin1 = A1;     // Direction Pin for motor01
-const int PulPin1 = A0;     // Step Pin for motor01
-
-const int endstop1 = 15;  // Endstop 1
-
-
-#define motor02 2
-
-const int enablePin2 = 46;  // Enable Pin
-const int DirPin2 = A7;     // Direction Pin for motor2
-const int PulPin2 = A6;     // Step Pin for motor2
-
-const int endstop2 = 14;  // Endstop 2
-
-
-#define motor03 3
-
-const int enablePin3 = A8;  // Enable Pin
-const int DirPin3 = 48;     // Direction Pin for motor3
-const int PulPin3 = 46;     // Step Pin for motor3
-
-const int endstop3 = 2;  // Endstop 3
-
-
-#define motor04 4
-
-const int enablePin4 = 24;  // Enable Pin
-const int DirPin4 = 28;     // Direction Pin for motor4
-const int PulPin4 = 26;     // Step Pin for motor4
-
-const int endstop4 = 3;  // Endstop 4
-
-bool initialize = 1;
-long steps = 0;
+int closingBoxes = false;
+int closeBoxesDelay = 2000;
+int lastMillisTimer = 0;
 
 /* Construct object, Embryo(Axis, Enable Pin, Direction Pin, Pulse Pin, Endstop Home, Endstop Far, Forward Button, Backward Button, Start Button, Emergency) */
-
-int motorAmount = 4;
-
-StepMotor box[4] = { StepMotor(motor01, enablePin1, DirPin1, PulPin1, endstop1, FarEndstop, ForwardPin, BackwardPin, startPin, emergencyPin),
-                     StepMotor(motor02, enablePin2, DirPin2, PulPin2, endstop2, FarEndstop, ForwardPin, BackwardPin, startPin, emergencyPin),
-                     StepMotor(motor03, enablePin3, DirPin3, PulPin3, endstop3, FarEndstop, ForwardPin, BackwardPin, startPin, emergencyPin),
-                     StepMotor(motor04, enablePin4, DirPin4, PulPin4, endstop4, FarEndstop, ForwardPin, BackwardPin, startPin, emergencyPin) };
-
 void setup() {
 
   Serial.begin(9600);  // Configure and start Serial Communication
 
   while (!Serial) {};
 
-  for (int i = 0; i < motorAmount; i++) {
-    box[i].setSpeed(1000);
+  for (int i = 0; i < motorCount; i++) {
+    pinMode(enablePin[i], OUTPUT);
+    pinMode(dirPin[i], OUTPUT);
+    pinMode(stepPin[i], OUTPUT);
+    pinMode(endstopPin[i], INPUT);
+    Serial.print("home Motor: ");
+    Serial.println(i);
+    enableMotor(i);
+    homeMotor(i);
   }
-
-  Serial.println("Homing complete");
-
-  // for (int i = 0; i < motorAmount; i++){
-  //   box[i].begin();
-  //   box[i].startWithoutHoming();
-
-  //   while(!box[i].readEndstopHome()){
-
-  //     box[i].moveBackward();
-  //   }
-  // }
-
-
-  // Serial.println("To start, send any key to serial ...");
-
-  // // Without this instruction the motor will move after the upload (It is dangerous)
-  // while((Serial.available() <= 0)){};
 }
 
-void loop() {
+void disableMotor (int m){
+  digitalWrite(enablePin[m], HIGH);
+}
+void enableMotor (int m){
+  digitalWrite(enablePin[m], LOW);
+}
 
-  if(initialize){
-    box[0].begin();
-    box[0].startWithoutHoming();
 
-    Serial.println("Homing");
-
-    while (!digitalRead(endstop1)) {
-      Serial.println(digitalRead(endstop1));
-      //box[0].moveBackward();
-    }
-
-    Serial.println("End");
-
-    box[0].pause();
-
-    initialize = 0;
+void homeMotor (int m){
+  digitalWrite(dirPin[m], HIGH);
+  motorMove[m] = mBackwards;
+  while(digitalRead(endstopPin[m])){ // Move motor until endstop is triggered
+      //Serial.println(digitalRead(enablePin[m]));
+      motorPulse[m] = !motorPulse[m];
+      digitalWrite(stepPin[m], motorPulse[m]);
+      delay(speedDelay);
   }
+  motorPosition[m] = 0;
+}
 
-  Serial.print("1: ");
-  Serial.print(digitalRead(endstop1) ? "high - " : "low - ");
+void setPosition (int m, int p){
+  targetPosition[m] = p; 
+}
 
-  Serial.print("2: ");
-  Serial.print(digitalRead(endstop2) ? "high - " : "low - ");
+void stepMotor (void){
+  
+  // Iterate through each motor
+  for (int i = 0; i < motorCount; i++) {
+   if(motorMove[i] == mForwards || motorMove[i] == mBackwards && // Check if the motor is supposed to be moving
+        lastStepTime[i] + speedDelay <= millis() && // Check if the last step was taken within the specified delay
+        (!digitalRead(endstopPin[i]) && motorMove[i] == mBackwards ||
+        digitalRead(endstopPin[i] && motorMove[i] == mForwards))) { 
+      lastStepTime[i] = millis();
+      motorPulse[i] = !motorPulse[i];
+      digitalWrite(stepPin[i], motorPulse[i]);
+      motorPosition[i] += motorMove[i];
+   }
 
-  Serial.print("3: ");
-  Serial.print(digitalRead(endstop3) ? "high - " : "low - ");
+   if(motorPosition[i] < targetPosition[i]){
+     digitalWrite(dirPin[i], LOW);
+     motorMove[i] = mForwards;
+   }
+   else if(motorPosition[i] > targetPosition[i]){
+     digitalWrite(dirPin[i], HIGH);
+     motorMove[i] = mBackwards;
+   }
+   else if(motorMove[i] != mStop) {
+    motorMove[i] = mStop;
+    Serial.println("!OK");
+   }
+  }
+}
+ 
+void loop() {
+  delay(5);
 
-  Serial.print("4: ");
-  Serial.print(digitalRead(endstop4) ? "high - " : "low - ");
-
-  Serial.println();
-
-  delay(100);
+  stepMotor();
 
   // Read from serial
   // Put result into buffer
   receiveBufferSize = 0;
-  while (Serial.available() > 0) {
-    receiveBuffer[receiveBufferSize] = char(Serial.read());
-    if (receiveBufferSize < buffSize - 1)
-      receiveBufferSize++;
+  if(Serial.available() > 3){
+      while (Serial.available() > 0) {
+      receiveBuffer[receiveBufferSize] = char(Serial.read());
+      if (receiveBufferSize < buffSize - 1) {
+        receiveBufferSize++;
+      }
+    }
   }
 
-  // Process buffer
+
+  // Process serial buffer
   receiveBuffer[receiveBufferSize] = 0;
   if (receiveBuffer[0] == '!')  // Check buffer starts with command symbol
   {
+    Serial.println("Processing command");
+
     // Remove command character
     String commandString = String(receiveBuffer);
     commandString.remove(0, 1);
@@ -148,19 +133,22 @@ void loop() {
     char *part = strtok(commandString.c_str(), ",");
 
     bool bucketPositions[] = { CLOSE, CLOSE, CLOSE, CLOSE };
+    int bucketPositionsLength = 4;
 
     while (part != NULL) {  // Process split parts
       int targetBox = 0;
       for (int i = 0; i < sizeof(part); i++) {
         switch (part[i]) {
-
           case 'O':  // Box should be opened
-            Serial.print("Opening box: ");
-            Serial.println(targetBox);
+            if(targetBox != -1){
+              bucketPositions[targetBox - 1] = OPEN; // Store desired position
+            }
             break;
+
           case 'C':  // Box should be closed
-            Serial.print("Closing box: ");
-            Serial.println(targetBox);
+            if(targetBox != -1){
+              bucketPositions[targetBox - 1] = CLOSE; // Store desired position
+            }
             break;
 
           default:                                   // Determine target box
@@ -168,7 +156,7 @@ void loop() {
             if (targetBox >= 1 && targetBox <= 4) {  // If targetBox is in expected range
               continue;
             } else {
-              targetBox = 0;
+              targetBox = -1;
             }
             break;
         }
@@ -177,5 +165,28 @@ void loop() {
       // Get the next part in the command string
       part = strtok(NULL, ",");
     }
+
+    move_boxes(bucketPositions, bucketPositionsLength);
   }
+}
+
+void move_boxes(bool positions[], int length){
+
+  for(int i = 0; i < length; i++){
+    if(positions[i] == OPEN){
+      open_box(i, openPuls);
+    }
+    if(positions[i] == CLOSE){
+      close_box(i);
+    }
+  }
+}
+
+void open_box(int m, int p) {
+  // Open the box
+  setPosition(m, p);
+}
+
+void close_box(int m){
+  setPosition(m, 0);
 }
